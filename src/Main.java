0c11bf90adbf455b91c12b61e41812b091fcc06a
Geraldo Alves <geraldoalves.sjunior@hotmail.com>
2024-01-16 22:09:23 -0300

    First class: Position

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import boardgame.Position;
+
 public class Main {
     public static void main(String[] args) {
 
+        Position position = new Position(5,6);
+        System.out.println(position);
     }
 }
